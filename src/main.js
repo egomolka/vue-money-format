68c54c2b1d3b2d4d2455a9f0ed2370e1041f2147
Nomaan <nomaan.aquarian@gmail.com>
2019-02-12 17:58:50 +0530

    added installation script

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
 import MoneyFormat from './components/MoneyFormat.vue';
 
+MoneyFormat.install = (Vue) => {
+  Vue.component(MoneyFormat.name, MoneyFormat);
+};
+
+if (typeof window !== 'undefined' && window.Vue) {
+  window.Vue.use(MoneyFormat);
+}
+
 export default MoneyFormat;
